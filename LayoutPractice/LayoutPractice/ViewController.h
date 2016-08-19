79204178edd27941cc8307a1633fec649ca0bd0a
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-08-19 18:58:26 +0800

    project initialization

---
+++ LayoutPractice/LayoutPractice/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  LayoutPractice
+//
+//  Created by LLDM on 19/08/2016.
+//  Copyright © 2016 LLDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
