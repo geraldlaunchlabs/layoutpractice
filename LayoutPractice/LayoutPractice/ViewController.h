ab8e77848441757479d83210d60cab390b902c51
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-08-23 13:43:28 +0800

    ver 1.0

--- LayoutPractice/LayoutPractice/ViewController.h
+++ LayoutPractice/LayoutPractice/ViewController.h
@@ -11,6 +11,14 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIView *profPic;
+@property (strong, nonatomic) IBOutlet UIButton *btn1;
+@property (strong, nonatomic) IBOutlet UIButton *btn2;
+@property (strong, nonatomic) IBOutlet UIButton *btn3;
+@property (strong, nonatomic) IBOutlet UIButton *btn4;
+@property (strong, nonatomic) IBOutlet UIButton *btn5;
+@property (strong, nonatomic) IBOutlet UILabel *lbl1;
+@property (strong, nonatomic) IBOutlet UILabel *lbl2;
 
 @end
 
--- LayoutPractice/LayoutPractice/ViewController.m
+++ LayoutPractice/LayoutPractice/ViewController.m
@@ -16,7 +16,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.btn1.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.btn2.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.btn3.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.btn4.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.btn5.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self setUI];
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    [self setUI];
+}
+
+- (void)setUI {
+    self.profPic.layer.cornerRadius = self.profPic.frame.size.width/2;
+    self.profPic.layer.borderWidth = self.profPic.frame.size.width/50;
+    self.profPic.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.profPic.clipsToBounds = YES;
+    self.lbl1.font = [UIFont systemFontOfSize:self.lbl1.frame.size.height];
+    self.lbl2.font = [UIFont systemFontOfSize:self.lbl2.frame.size.height];
+    
 }
 
 - (void)didReceiveMemoryWarning {
